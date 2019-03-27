036d24c9d992841a2a6444d9989b591ddc2af8f1
github username <github email address>
2019-03-27 15:47:46 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,21 @@
 class ArtistsController < ApplicationController
+
+    def show
+        @artist = Artist.find_by(params[:id])
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.create(params.require(:artists).permit(:name, :bio))
+        redirect_to artist_path(@artist)
+    end
+
+    def update
+        @artist = Artist.find_by(params[:id])
+        @artist.update(params.require(:artists).permit(:name, :bio)
+        redirect_to artist_path(@artist)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :artists, only: [:show, :create, :update]
-  resources :genres, only: [:show, :create, :update]
-  resources :songs, only: [:show, :create, :update]
+  resources :artists, only: [:new, :show, :create, :update]
+  resources :genres, only: [:new, :show, :create, :update]
+  resources :songs, only: [:new, :show, :create, :update]
   
 end
