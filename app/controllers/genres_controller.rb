7be7bf4a08a51f6c4cf7cd7193eca8069c12c5ab
Jane Venter <janeanneventer@gmail.com>
2019-04-12 18:33:30 -0700

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,29 @@
+class GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def create
+    @genre = Genre.create(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre.id)
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(params.require(:genre).permit(:name))
+    @genre.save 
+    redirect_to genre_path(@genre.id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,39 @@
 Rails.application.routes.draw do
+  get 'songs/index' => 'songs#index', :as => :songs
+
+  get 'songs/:id' => 'songs#show', :as => :song
+
+  get 'songs/new' => 'songs#new', :as => :new_song
+
+  post 'songs/create' => 'songs#create'
+
+  get 'songs/edit' => 'songs#edit', :as => :edit_song
+
+  put 'songs/update' => 'songs#update'
+
+  get 'genres/index' => 'genres#index', :as => :genres
+
+  get 'genres/:id' => 'genres#show', :as => :genre
+
+  get 'genres/new' => 'genres#new', :as => :new_genre
+
+  post 'genres/create' => 'genres#create'
+
+  get 'genres/edit' => 'genres#edit', :as => :edit_genre
+
+  put 'genres/update' => 'genres#update'
+
+  get 'artists/index' => 'artists#index', :as => :artists
+
+  get 'artists/:id' => 'artists#show', :as => :artist
+
+  get 'artists/new' => 'artists#new', :as => :new_artist
+
+  post 'artists/create' => 'artists#create'
+
+  get 'artists/edit' => 'artists#edit', :as => :edit_artist
+
+  put 'artists/update' => 'artists#update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
